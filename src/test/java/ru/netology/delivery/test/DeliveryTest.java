764a7ca90326dca6f63f211d6b99fb14ac9c5470
Yaroshenko Natalya <natavik2401@gmail.com>
2025-04-27 20:55:38 +0300

    change project2

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,11 +1,11 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import ru.netology.delivery.data.DataGenerator;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -15,6 +15,16 @@ import static com.codeborne.selenide.Selenide.*;
 
 
 class DeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @BeforeEach
     void setup() {
